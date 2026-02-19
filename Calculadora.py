9512a0753370cade5edbbd385b8ef2fcfbbb4698
jacobocano <jacobo.cano@gmail.com>
2026-02-19 16:27:35 +0000

    st.login("microsoft")

--- Calculadora.py
+++ Calculadora.py
@@ -80,7 +80,8 @@ if st.session_state.get("authenticated"):
         st.logout()
 else:
     if st.sidebar.button("Iniciar sesión en Microsoft", on_click=st.login):
-        st.login()
+        st.login("microsoft")
+        #st.login()
         #st.login(provider="microsoft")
         
         #st.write(f"{st.user["name"]} - {st.user["email"]}")
